98b92dfbd18da0bcacd5571f9d708bf25050747d
ggako <19727160+ggako@users.noreply.github.com>
2023-07-26 02:18:04 +0800

    Add input statement test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+# This is the main program
+# test2.py is the library
+
+# import distance from test2
+
+
+
+x1 = float(input('What is the x coordinate of the first point? '))
+y1 = float(input('What is the y coordinate of the first point? '))
+x2 = float(input('What is the x coordinate of the second point? '))
+y2 = float(input('What is the y coordinate of the second point? '))
+
+print(x1,y1)
+print(x2,y2)
